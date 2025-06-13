93cb5351c76a43e02d9c50dcb45421b1db6d0d76
Aaditya Aanand <147561897+Aaditya514@users.noreply.github.com>
2025-06-13 10:29:35 +0530

    Update LikeButton.jsx

--- LikeButton.jsx
+++ LikeButton.jsx
@@ -2,18 +2,17 @@ import { useState } from "react";
 
 function LikeButton() {
     const [likes, setLikes] = useState(0);
-    const heartColor = likes > 0 ? 'text-red-500' : 'text-gray-500';
     return (
-        <div className="items-center justify-center flex flex-col shadow-lg rounded-xl p-6  border border-gray-200  w-64">
+        <div className="items-center justify-center flex flex-col shadow-lg rounded-xl p-6  border border-gray-200  w-84">
             <h1 className="text-3xl  m-10 text-white text-center flex flex-row font-bold p-10">Like Button</h1>
             <div className="p-6 bg-white shadow-lg rounded-lg flex flex-row items-center justify-center ">
                 <button
                     onClick={() => setLikes(likes + 1)}
                     className="flex items-center space-x-2 px-4 py-2 bg-red-100 rounded-lg hover:bg-red-200 active:scale-95 transition-transform">
-                    <span className={`text-2xl transition-colors duration-300 ${heartColor}`}>
+                    <span className={`text-2xl transition-colors duration-300 `}>
                         ❤️
                     </span>
-                    <span className="font-semibold text-lg text-red-600">{likes}</span>
+                    <span className={`font-semibold ${likes > 0 ? 'text-red-600' : 'text-black'}`}>{likes} Click Me</span>
                 </button>
 
                 <button
